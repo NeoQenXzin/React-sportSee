011e81295e98c1a692e6434591a4f1dedd2d7fb1
Dragonk1 <keyntao99@yahoo.fr>
2022-10-23 20:33:08 +0200

    ajout du BarChart css et jsx

--- src/components/BarChart/BarChart.jsx
+++ src/components/BarChart/BarChart.jsx
@@ -1,28 +1,71 @@
 import React from "react";
+import "./BarChart.css";
 import {
   BarChart,
   CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
-  Legend,
   Bar,
+  ResponsiveContainer,
 } from "recharts";
 import { USER_ACTIVITY } from "../../data/mocked-data";
 
 export default function BarChartx() {
+  const formatDay = (item) => new Date(item).getDate();
   return (
     <div>
-      BarChart
-      <BarChart width={835} height={320} data={USER_ACTIVITY[1].sessions}>
-        <CartesianGrid strokeDasharray="2" vertical={false} />
-        <XAxis dataKey="day" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="kilogram" fill="#8884d8" />
-        <Bar dataKey="calories" fill="#82ca9d" />
-      </BarChart>
+      <h2 className="barchart-title">Activité quotidienne</h2>
+      <ul className="barchart-legend">
+        <li>Poids (kg)</li>
+        <li>Calories brûlées (kcal)</li>
+      </ul>
+      <ResponsiveContainer width={835} height={200}>
+        <BarChart data={USER_ACTIVITY[1].sessions}>
+          <CartesianGrid strokeDasharray="2" vertical={false} />
+          <XAxis
+            dataKey="day"
+            tickMargin={16}
+            tickSize={0}
+            tickFormatter={formatDay}
+            // minTickGap={30}
+          />
+          <YAxis
+            dataKey="kilogram"
+            orientation="right"
+            tickMargin={40}
+            tickSize={0}
+            axisLine={false}
+            domain={["dataMin-2", "dataMax+1"]}
+            // interval={"preserveEnd"}
+            tickCount={3}
+          />
+          <YAxis
+            dataKey="calories"
+            yAxisId="calories"
+            hide
+            orientation="right"
+            domain={["dataMin-100", "dataMax+0"]}
+          />
+          <Tooltip />
+          <Bar
+            dataKey="kilogram"
+            fill="#282d30"
+            radius={[5, 5, 0, 0]}
+            barSize={10}
+          />
+          {/* Bar de separation transparent */}
+          <Bar dataKey="calories" fill="transparent" barSize={3} />
+          {/* ---- */}
+          <Bar
+            yAxisId="calories"
+            dataKey="calories"
+            fill="#e60000"
+            radius={[5, 5, 0, 0]}
+            barSize={10}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
